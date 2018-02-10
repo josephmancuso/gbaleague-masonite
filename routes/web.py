b6914ea2d77627ada1352f774c053bafabb44a71
Joseph Mancuso <idmann509@gmail.com>
2018-02-10 15:24:53 -0500

    added routes

--- routes/web.py
+++ routes/web.py
@@ -3,4 +3,18 @@ from masonite.routes import Get, Post
 
 ROUTES = [
     Get().route('/', 'WelcomeController@show').name('home'),
+    Get().route('/league/@id', 'LeagueController@show').name('league'),
+    Get().route('/league/@id/teams', 'LeagueController@teams').name('league-teams'),
+    Get().route('/league/@id/draft', 'DraftController@show').name('league-draft'),
+    Post().route('/league/@id/draft', 'DraftController@draft'),
+]
+
+# Authentication Routes
+ROUTES = ROUTES + [
+    Get().route('/login', 'LoginController@show'),
+    Get().route('/logout', 'LoginController@logout'),
+    Post().route('/login', 'LoginController@store'),
+    Get().route('/register', 'RegisterController@show'),
+    Post().route('/register', 'RegisterController@store'),
+    Get().route('/home', 'HomeController@show'),
 ]
